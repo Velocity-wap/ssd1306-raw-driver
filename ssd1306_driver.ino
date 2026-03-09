e3f16af61e5d7280b8cb76312a0a1cdc17cb3bc0
Swapnil Baruah <109682678+Velocity-wap@users.noreply.github.com>
2026-03-09 20:50:19 +0530

    Add files via upload

--- ssd1306_driver.ino
+++ ssd1306_driver.ino
@@ -1,14 +1,24 @@
 #include <Wire.h>
 #include "ssd1306.h"
+#include <string.h>
+int x = 62;
 
 void setup(){
-  Wire.begin();
-  ssd1306_init();
+    Wire.begin();
+    ssd1306_init();
+    ssd1306_clear();
+    ssd1306_update();
 }
 
 void loop(){
-  ssd1306_write_command(0xAF);
-  delay(1000);
-  ssd1306_write_command(0xAE);
+  ssd1306_clear();
+  set_pixel(x, 31);
+  set_pixel(x, 32);
+  set_pixel(x, 33);
+  ssd1306_update();
+  x += 1;
+  if (x > 67){
+    x = 62;
+  }
   delay(1000);
 }
