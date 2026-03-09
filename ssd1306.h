1c8ca18a3ec0da84e628ac2bddd07983187e9ea0
Swapnil Baruah <109682678+Velocity-wap@users.noreply.github.com>
2026-03-09 21:04:55 +0530

    Add SSD1306 bare-metal driver with framebuffer support

--- ssd1306.h
+++ ssd1306.h
@@ -1,11 +1,12 @@
 #ifndef SSD1306_H
 #define SSD1306_H
 #include <Arduino.h>
+extern uint8_t ssd1306_buffer[1024];
 
 void ssd1306_init();
 void ssd1306_write_command(uint8_t cmd);
-extern uint8_t ssd1306_buffer[1024];
 void ssd1306_clear();
 void set_pixel(uint8_t x, uint8_t y);
 void ssd1306_update();
+
 #endif
